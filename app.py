c473317e6fc4d7f1d91940f8b7cb039b2715d7cf
prezgarfield <robccnmtl@gmail.com>
2019-02-11 11:12:22 -0500

    having trouble with using an alternative directory to read from, but added Path functionality to each averaging/medianing function

--- app.py
+++ app.py
@@ -32,8 +32,10 @@ def getVidCap(fname):
     cap.release()
     cv2.destroyAllWindows()
 
-def MedianPixels3():
+def MedianPixels3(pth):
+
     allfiles = os.listdir(os.getcwd())
+    #allfiles = os.listdir(pth)
     print(allfiles)
     imlist = [filename for filename in allfiles if filename[-4:] in [".jpg"]]
 
@@ -44,7 +46,7 @@ def MedianPixels3():
     out.save("median.jpg")
     out.show()
 
-def AveragePixels2():
+def AveragePixels2(pth):
     allfiles = os.listdir(os.getcwd())
     print(allfiles)
     imlist = [filename for filename in allfiles if filename[-4:] in [".jpg"]]
@@ -56,7 +58,7 @@ def AveragePixels2():
     out.save("mean.jpg")
     out.show()
 
-def AveragePixels():
+def AveragePixels(pth):
     # Access all jpg files in directory
     pth = Path('data')
 
@@ -74,31 +76,13 @@ def AveragePixels():
     avg.save("Blend.png")
     avg.show()
 
-    # #print(imlist)
-    # # Assuming all images are the same size, get dimensions of first image
-    # w,h=Image.open(imlist[0]).size
-    # N=len(imlist)
-    #
-    # # Create a numpy array of floats to store the average (assume RGB images)
-    # arr=numpy.zeros((h,w,3),numpy.float)
-    #
-    # # Build up average pixel intensities, casting each image as an array of floats
-    # i = 0
-    # for im in imlist:
-    #     imarr = numpy.array(Image.open(im), dtype=numpy.float)
-    #     arr = arr + imarr / N
-    #
-    # # Round values in array and cast as 8-bit integer
-    # arr=numpy.array(numpy.round(arr),dtype=numpy.uint8)
-    #
-    # # Generate, save and preview final image
-    # out=Image.fromarray(arr,mode="RGB")
-    # out.save("Average.jpg")
-    # out.show()
+def setpath(pth):
+    mypath = Path(pth)
+    return mypath
 
 if __name__ == '__main__':
     amIrunning()
-    AveragePixels2()
-    MedianPixels3()
-    getVidCap('driveclip1.mp4')
+    AveragePixels2(setpath('data'))
+    #MedianPixels3(setpath('data'))
+    #getVidCap('driveclip1.mp4')
     exit()
